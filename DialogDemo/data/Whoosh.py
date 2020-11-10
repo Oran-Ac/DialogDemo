eaaaf91b7ecbbf9579b17c801110ea1b2ddc65e2
OranAc <994347705@qq.com>
2020-11-10 11:37:32 +0000

    fix the bug in Whoosh.py

--- DialogDemo/data/Whoosh.py
+++ DialogDemo/data/Whoosh.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
 
 
 import os
@@ -10,9 +6,8 @@ from whoosh.fields import *
 from jieba.analyse import ChineseAnalyzer
 from whoosh.writing import AsyncWriter
 import json
+from whoosh.index import open_dir
 
-
-# In[4]:
 
 
 #创建scheam对象
@@ -25,104 +20,30 @@ scheam = Schema(
                                 keywords = TEXT(stored = True,analyzer = ChineseAnalyzer()) )
 
 
-# In[5]:
 
 
 #创建schema的索引
 if not os.path.exists("index"):
     os.mkdir("index")
-idx = create_in("index",scheam)
+    idx = create_in("index",scheam)
 
 
-# In[6]:
+for path in ['test','train','valid']:
+    file_path = "./LCCC/LCCC-base_"+path+".json" #导入json文件
+    with open(file_path,'rb') as f:
+        data = json.load(f)
+    print(f'the data set contains: # {len(data)}')
+    idx = open_dir("index")
+    for i in range(0,len(data)-1):
+        for utter in data[i]:
+            writer = idx.writer()
+            utter =  utter.replace(" ","") #去掉json中的空格
+            writer.add_document(utter = utter,keywords = utter )
+            writer.commit()
+    idx.close()
 
-
-file_path = "./LCCC/LCCC-base_train.json" #导入json文件
-
-
-# In[7]:
-
-
-with open(file_path,'rb') as f:
-    data = json.load(f)
-
-
-# In[8]:
-
-
-print(f'the data set contains: # {len(data)}')
-
-
-# In[9]:
-
-
-writer = AsyncWriter(index=idx)
 '''
-由于不是很清楚后面retrieval的时候到底怎么存，所以这里先导入1001条，方便后续修改
 此处的导入方式：一条存一句
 ''' 
-for i in range(0,1000):
-    for utter in data[i]:
-        utter =  utter.replace(" ","") #去掉json中的空格
-        writer.add_document(utter = utter,keywords = utter )
-writer.commit()
-
-
-# In[ ]:
-
-
-# 采用indexWriter写入数据
-#writer = AsyncWriter(index=idx)
-#for utters in data:
-#    for utter in data:
-#        writer.add_document(utter = utter,keywords = utter )
-#writer.commit()
-
-
-# In[61]:
-
-
-# =============查找测试================
-from whoosh.qparser import QueryParser
-parser = QueryParser("keyword", idx.schema)
-
-
-# In[69]:
-
-
-with idx.searcher() as searcher:
-    query = parser.parse(u"回")
-    results = searcher.search(query)
-
-
-# In[71]:
-
-
-for i in range(min(10,len(result))):
-    print(result[i])
-
-
-# In[67]:
-
-
-len(result)
-#============查找测试结束==================
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
